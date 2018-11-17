d1832232ecea0b9b54abc92e24d17402bd0204e0
shrutikarva <shruti.karva@gmail.com>
2018-11-17 09:21:33 +0400

    initial commits

---
+++ SocialLacasa/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialLacasa.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Account()
+        {
+            return View();
+        }
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+        public ActionResult Orders()
+        {
+            return View();
+        }
+        public ActionResult MassOrders()
+        {
+            return View();
+        }
+        public ActionResult AddFunds()
+        {
+            return View();
+        }
+        public ActionResult Subscriptions()
+        {
+            return View();
+        }
+        public ActionResult NewOrder()
+        {
+            return View();
+        }
+    }
+}
---
+++ SocialLacasa/Controllers/VisitorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialLacasa.Controllers
+{
+    public class VisitorController : Controller
+    {
+        // GET: Visitor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        public ActionResult Terms()
+        {
+            return View();
+        }
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+    }
+}
